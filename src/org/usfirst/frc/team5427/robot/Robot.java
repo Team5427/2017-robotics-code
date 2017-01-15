1115aa6bcc2fc353e1950687c9f4490defa33b28
Blake Romero <blake.romero724@gmail.com>
2017-01-15 14:45:45 -0600

    misc changes

--- src/org/usfirst/frc/team5427/robot/Robot.java
+++ src/org/usfirst/frc/team5427/robot/Robot.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team5427.robot.OurClasses.*;
 
-import org.usfirst.frc.team5427.robot.commands.ExampleCommand;
+//import org.usfirst.frc.team5427.robot.commands.ExampleCommand;
 import org.usfirst.frc.team5427.robot.util.Log;
 import org.usfirst.frc.team5427.robot.util.Config;
 
@@ -77,7 +77,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
+		//chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
 		
