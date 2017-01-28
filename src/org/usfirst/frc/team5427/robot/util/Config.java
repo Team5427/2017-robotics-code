34ca1e9e7a85f239876427ddf056ab9c64d18007
Charlemagne Wong <frian13@hotmail.com>
2017-01-28 15:28:36 -0600

    Add config for motor and shooter offsets and bias

--- src/org/usfirst/frc/team5427/robot/util/Config.java
+++ src/org/usfirst/frc/team5427/robot/util/Config.java
@@ -58,16 +58,20 @@ public class Config {
 	public static final double BIAS_REAR_LEFT_MOTOR 	= 1;
 	public static final double BIAS_FRONT_RIGHT_MOTOR 	= 1;
 	public static final double BIAS_REAR_RIGHT_MOTOR 	= 1;	
-	public static final double BIAS_SHOOTER_MOTOR 		= 1;
-	public static final double BIAS_INTAKE_MOTOR 		= 1;
+	public static final double BIAS_SHOOTER_MOTOR_FORWARD 		= 1;
+	public static final double BIAS_SHOOTER_MOTOR_BACKWARD 		= 1;
+	public static final double BIAS_INTAKE_MOTOR_FORWARD 		= 1;
+	public static final double BIAS_INTAKE_MOTOR_BACKWARD 		= 1;
 	
 	// Motor Offset
 	public static final double OFFSET_FRONT_LEFT_MOTOR 	= 0;
 	public static final double OFFSET_REAR_LEFT_MOTOR 	= 0;
 	public static final double OFFSET_FRONT_RIGHT_MOTOR = 0;
 	public static final double OFFSET_REAR_RIGHT_MOTOR 	= 0;
-	public static final double OFFSET_SHOOTER_MOTOR 	= 0;
-	public static final double OFFSET_INTAKE_MOTOR 		= 0;
+	public static final double OFFSET_SHOOTER_MOTOR_FORWARD 	= 0;
+	public static final double OFFSET_SHOOTER_MOTOR_BACKWARD 	= 0;
+	public static final double OFFSET_INTAKE_MOTOR_FORWARD 		= 0;
+	public static final double OFFSET_INTAKE_MOTOR_BACKWARD 	= 0;
 	
 	//motor speeds
 	public static final double INTAKE_MOTOR_SPEED 	= .45;
