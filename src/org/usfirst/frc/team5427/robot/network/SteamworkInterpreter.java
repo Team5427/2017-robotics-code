f435e2621e9696aeae76f27447d47daa01a0b3f3
Charlemagne Wong <frian13@hotmail.com>
2017-03-09 19:48:16 -0600

    Update from vp

--- src/org/usfirst/frc/team5427/robot/network/SteamworkInterpreter.java
+++ src/org/usfirst/frc/team5427/robot/network/SteamworkInterpreter.java
@@ -6,10 +6,23 @@ import org.usfirst.frc.team5427.robot.util.Log;
 
 import java.util.ArrayList;
 
+///////////////////////////////////////////////////////////////////////////////////////////
 
 public class SteamworkInterpreter extends Interpreter {
 
     public volatile ArrayList<Object> recievedObjects = new ArrayList<>();
+
+    public volatile long lastRecievedTime = 0;
+
+    /**
+     * Horizontal angle received from the network
+     */
+    public volatile double horizontalAngle = 0;
+
+    /**
+     * Distance received from the network
+     */
+    public volatile double distance = 0;
 
     @Override
     public void interpret(byte[] buff, int numFromStream) {
@@ -27,6 +40,8 @@ public class SteamworkInterpreter extends Interpreter {
             case ByteDictionary.OBJECT:
                 recievedObjects.add( deserialize(buff, 1, buff.length - 1) );
                 break;
+            case ByteDictionary.TARGET_DATA:
+                // Code here
             default:
                 Log.debug("Invalid dictionary: " + toStringByteArray(buff));
                 break;
