54d6164a173005ffb9172d0ac2b7f506363afe51
Charlemagne Wong <frian13@hotmail.com>
2017-03-11 14:02:27 -0600

    Fix missing code

--- src/org/usfirst/frc/team5427/robot/OurClasses/SteelPIDOutput.java
+++ src/org/usfirst/frc/team5427/robot/OurClasses/SteelPIDOutput.java
@@ -1,5 +1,29 @@
 package org.usfirst.frc.team5427.robot.OurClasses;
 
-public class SteelPIDOutput {
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PWMSpeedController;;
 
+/**
+ * Allows multiple PWMSpeedControllers for a single PIDOutput
+ * 
+ * @author Charlemagne Wong
+ *
+ */
+public class SteelPIDOutput implements PIDOutput{
+
+	PWMSpeedController[] controllers;
+	
+	public SteelPIDOutput(PWMSpeedController... controllers)
+	{
+		this.controllers = controllers;
+	}
+	
+	@Override
+	public void pidWrite(double output) {
+		// TODO Auto-generated method stub
+		for (PWMSpeedController i : controllers) {
+			i.pidWrite(output);
+		}
+	}
+	
 }
